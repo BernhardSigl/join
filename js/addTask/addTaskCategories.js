0941a36477cad4faa44f1bad541c37b254db8ab9
Bernhard Sigl <b-sigl@live.de>
2023-12-10 16:14:48 +0100

    addTask -> Categories: jsdoc

--- js/addTask/addTaskCategories.js
+++ js/addTask/addTaskCategories.js
@@ -1,4 +1,6 @@
-// category
+/**
+ * Handles the dropdown functionality for task categories.
+ */
 function categoryDropdown() {
     disableCategoryInput();
     let dropdownImg = document.getElementById('categoryDropdownImgId');
@@ -7,6 +9,9 @@ function categoryDropdown() {
     changeDropdownFunction();
 }
 
+/**
+ * Handles the dropup functionality for task categories.
+ */
 function categoryDropup() {
     let dropupImg = document.getElementById('categoryDropdownImgId');
     toggleVisibility('categoryDropdownContentId', false);
@@ -14,6 +19,9 @@ function categoryDropup() {
     originalDropdownFunction();
 }
 
+/**
+ * Changes the click event for the category dropdown button to dropup.
+ */
 function changeDropdownFunction() {
     let dropdownBtn = document.getElementById('categoryDropdownImgId');
     dropdownBtn.onclick = function () {
@@ -21,6 +29,9 @@ function changeDropdownFunction() {
     };
 }
 
+/**
+ * Changes the click event for the category dropdown button to dropdown.
+ */
 function originalDropdownFunction() {
     let addCategoryBtn = document.getElementById('categoryDropdownImgId');
     addCategoryBtn.onclick = function () {
@@ -28,6 +39,9 @@ function originalDropdownFunction() {
     };
 }
 
+/**
+ * Lists task categories and updates the category list.
+ */
 async function listCategories() {
     let categoryInput = document.getElementById('addCategoryId');
     let categoryList = document.getElementById('categoryListId');
@@ -38,11 +52,20 @@ async function listCategories() {
     }
 }
 
+/**
+ * Displays an error message for invalid category input.
+ * @param {HTMLInputElement} categoryInput - The category input element.
+ */
 function invalidCategoryInput(categoryInput) {
     categoryInput.setCustomValidity('Please enter at least one character.');
     categoryInput.reportValidity();
 }
 
+/**
+ * Processes valid category input and updates the category list.
+ * @param {HTMLInputElement} categoryInput - The category input element.
+ * @param {HTMLElement} categoryList - The category list element.
+ */
 async function validCategoryInput(categoryInput, categoryList) {
     categoryInput.setCustomValidity('');
     categoriesInTaskArray.push(categoryInput.value);
@@ -56,6 +79,9 @@ async function validCategoryInput(categoryInput, categoryList) {
     await setItem(`individuallyCategories_${userId}`, JSON.stringify(categoriesInTaskArray));
 }
 
+/**
+ * Checks the empty status of the category list and adjusts visibility.
+ */
 function checkCategoryEmptyStatus() {
     let addCategoryImg = document.getElementById('addCategoryBtnId');
     if (categoriesInTaskArray.length == 0) {
@@ -65,6 +91,10 @@ function checkCategoryEmptyStatus() {
     }
 }
 
+/**
+ * Adjusts styling and visibility when there are no categories stored.
+ * @param {HTMLElement} addCategoryImg - The "add category" button image.
+ */
 function withoutCategoriesStorage(addCategoryImg) {
     addCategoryImg.style.setProperty('right', '0', 'important');
     toggleVisibility('categoryDropdownImgId', false);
@@ -72,12 +102,20 @@ function withoutCategoriesStorage(addCategoryImg) {
     categoryDropup();
 }
 
+/**
+ * Adjusts styling and visibility when there are categories stored.
+ * @param {HTMLElement} addCategoryImg - The "add category" button image.
+ */
 function withCategoriesStorage(addCategoryImg) {
     addCategoryImg.style.setProperty('right', '50px', 'important');
     toggleVisibility('categoryDropdownImgId', true);
     toggleVisibility('categoryGreylineId', true);
 }
 
+/**
+ * Sets the selected category in the input field when a category is clicked.
+ * @param {number} i - The index of the selected category.
+ */
 function addCategory(i) {
     let category = categoriesInTaskArray[i];
     let categoryInput = document.getElementById('addCategoryId');
@@ -85,6 +123,10 @@ function addCategory(i) {
     categoryDropup();
 }
 
+/**
+ * Edits a task category, allowing renaming.
+ * @param {number} i - The index of the category to edit.
+ */
 async function editCategory(i) {
     let categoryInput = document.getElementById(`inputFieldCategory${i}`);
     let pencilImage = document.getElementById(`editCategoryImgID${i}`);
@@ -100,6 +142,11 @@ async function editCategory(i) {
     await setItem(`individuallyCategories_${userId}`, JSON.stringify(categoriesInTaskArray));
 }
 
+/**
+ * Confirms the renaming of a task category.
+ * @param {HTMLElement} pencilImage - The pencil icon image element.
+ * @param {number} i - The index of the category to confirm renaming.
+ */
 async function confirmCategoryRenaming(pencilImage, i) {
     doNotClose(event);
     pencilImage.style.backgroundImage = 'url("../img/pencilDark.png")';
@@ -113,28 +160,49 @@ async function confirmCategoryRenaming(pencilImage, i) {
     await setItem(`individuallyCategories_${userId}`, JSON.stringify(categoriesInTaskArray));
 }
 
+/**
+ * Reverts the category editing functionality to the original state.
+ * @param {HTMLElement} pencilImage - The pencil icon image element.
+ * @param {number} i - The index of the category being edited.
+ */
 function originalEditCategoryFunction(pencilImage, i) {
     pencilImage.onclick = function () {
         editCategory(i);
     };
 }
 
+/**
+ * Changes the edit category functionality to confirm renaming.
+ * @param {HTMLElement} pencilImage - The pencil icon image element.
+ * @param {number} i - The index of the category being edited.
+ */
 function changeEditCategoryFunction(pencilImage, i) {
     pencilImage.onclick = function () {
         confirmCategoryRenaming(pencilImage, i);
     };
 }
 
+/**
+ * Enables input for editing a category.
+ * @param {number} i - The index of the category input to enable.
+ */
 function enableCategoryInput(i) {
     document.getElementById(`inputFieldCategory${i}`).style.pointerEvents = 'auto';
 }
 
+/**
+ * Disables input for editing a category.
+ */
 function disableCategoryInput() {
     document.querySelectorAll('.categoryContent').forEach(function (element) {
         element.style.pointerEvents = 'none';
     });
 }
 
+/**
+ * Deletes a task category.
+ * @param {number} i - The index of the category to delete.
+ */
 async function deleteCategory(i) {
     doNotClose(event);
     categoriesInTaskArray.splice(i, 1);
@@ -144,6 +212,9 @@ async function deleteCategory(i) {
     await setItem(`individuallyCategories_${userId}`, JSON.stringify(categoriesInTaskArray));
 }
 
+/**
+ * Updates the displayed task category list.
+ */
 function updateCategoryList() {
     let categoryList = document.getElementById('categoryListId');
     categoryList.innerHTML = '';
