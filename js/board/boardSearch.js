e1ec13a4dc9cbf655b8ba131b1cd5cba30a3bc96
Bernhard Sigl <b-sigl@live.de>
2023-12-11 13:39:53 +0100

    board -> search: jsdoc

--- js/board/boardSearch.js
+++ js/board/boardSearch.js
@@ -1,3 +1,8 @@
+/**
+ * Filters tasks based on the provided search term.
+ * If the search term is empty, the function resets the search and displays all tasks.
+ * @param {string} searchTerm - The search term to filter tasks.
+ */
 function filterTasks(searchTerm) {
     if (searchTerm.trim() === '') {
         resetSearch();
@@ -8,10 +13,17 @@ function filterTasks(searchTerm) {
     showFilteredTasks();
 }
 
+/**
+ * Converts the search term and filters tasks that match the title, category, or description.
+ * @param {string} searchTerm - The search term to filter tasks.
+ */
 function searchToLowerCase(searchTerm) {
     filteredTasksArray = taskArray.filter(task => task.title.toLowerCase().includes(searchTerm.toLowerCase()) || task.category.toLowerCase().includes(searchTerm.toLowerCase()) || task.description.toLowerCase().includes(searchTerm.toLowerCase()));
 }
 
+/**
+ * Hides all tasks in the taskArray and category headers during the search.
+ */
 function hideNonFilteredTasks() {
     taskArray.forEach((_, index) => {
         const taskElement = document.getElementById(`taskId${index}`);
@@ -23,6 +35,9 @@ function hideNonFilteredTasks() {
     document.getElementById('doneSearchId').style.display = 'none';
 }
 
+/**
+ * Displays tasks that match the search criteria and shows the corresponding category headers.
+ */
 function showFilteredTasks() {
     filteredTasksArray.forEach(task => {
         const index = taskArray.indexOf(task);
@@ -33,6 +48,9 @@ function showFilteredTasks() {
     });
 }
 
+/**
+ * Resets the search, displaying all tasks and category headers.
+ */
 function resetSearch() {
     document.getElementById('searchFieldTasksId').value = '';
     taskArray.forEach((_, index) => {
